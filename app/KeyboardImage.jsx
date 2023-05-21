8db8f75b03350425f435f77dea88a9f4bfb7886d
gmo93 <omohundrogrant@yahoo.com>
2023-05-21 07:09:43 -0500

    Start of freelance website. So far, I have made the text being typed out, and the keys being pressed, but there are some kinks to work out. Not all letters show up as buttons are pressed, and not all buttons become unpressed.

---
+++ app/KeyboardImage.jsx
@@ -0,0 +1,290 @@
+import styles from './styles/Home.module.css';
+
+export default function KeyboardImage() {
+    return (
+        <svg id="Layer_2" viewBox="0 0 906.15 324.49">
+    {/* // <defs>
+    //     <style>.cls-1{letter-spacing:-.01em;}.cls-2{letter-spacing:-.01em;}.cls-3{fill:#facc15;stroke:#231f20;stroke-miterlimit:10;}.cls-4{letter-spacing:-.06em;}.cls-5{font-size:22px;}.cls-5,.cls-6{fill:#231f20;font-family:DINCondensed-Bold, 'DIN Condensed';font-weight:700;}.cls-6{font-size:18px;}</style>
+    // </defs> */}
+    <g id="Layer_3">
+        <rect className={styles.cls-3  x="58.62" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="124.47" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="190.97" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="257.48" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="323.99" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="389.83" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="454.35" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="521.52" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="588.03" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="654.21" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="720.71" y="4.36" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="786.89" y="4.36" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="853.11" y="5.11" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="2.58" y="5.11" width="42.42" height="56.03"/>
+        <rect className={styles.cls-3  x="64.28" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="130.12" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="196.63" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="263.14" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="329.64" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="395.49" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="460" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="527.17" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="593.68" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="659.86" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="725.67" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="858.02" y="70.44" width="47.59" height="56.53"/>
+        <rect className={styles.cls-3  x="792.85" y="70.44" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="2.58" y="70.44" width="48.06" height="56.53"/>
+        <rect className={styles.cls-3  x="78.24" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="144.09" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="210.59" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="277.1" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="343.61" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="409.45" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="473.97" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="541.14" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="607.65" y="135.77" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="673.07" y="136.02" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="739.58" y="136.02" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="805" y="136.52" width="99.55" height="56.53"/>
+        <rect className={styles.cls-3  x="2.58" y="135.77" width="61.7" height="56.53"/>
+        <text className={styles.cls-5  transform="translate(95.38 169.64)">
+            <tspan x="0" y="0">A</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(161.23 170.14)">
+            <tspan x="0" y="0">S</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(225.94 169.64)">
+            <tspan x="0" y="0">D </tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(292.73 170.14)">
+            <tspan x="0" y="0">F</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(358.29 170.14)">
+            <tspan x="0" y="0">G </tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(426.8 170.14)">
+            <tspan x="0" y="0">H</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(492.43 169.64)">
+            <tspan x="0" y="0">J</tspan>
+        </text>
+        <rect className={styles.cls-3  x="116.83" y="202.28" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="2.58" y="202.1" width="99.55" height="56.53"/>
+        <rect className={styles.cls-3  x="182.68" y="202.28" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="249.18" y="202.28" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="315.69" y="202.28" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="382.2" y="202.28" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="448.04" y="202.28" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="513.82" y="201.61" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="579.6" y="201.61" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="645.44" y="201.61" width="52.54" height="56.53"/>
+        <rect className={styles.cls-3  x="711.92" y="201.61" width="52.54" height="56.53"/>
+        <text className={styles.cls-5  transform="translate(134.78 236.15)">
+            <tspan x="0" y="0">Z</tspan>
+        </text>
+        <text className={styles.cls-5 shiftKey" transform="translate(29.52 239.47)">
+            <tspan x="0" y="0">Shift</tspan>
+        </text>
+        <text className={styles.cls-5 tabKey" transform="translate(13.28 104.81)">
+            <tspan className={styles.cls-4  x="0" y="0">T</tspan>
+            <tspan x="5.94" y="0">ab</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(9.91 161.88)">
+            <tspan x="0" y="0">Caps</tspan>
+            <tspan x=".34" y="20">Lo</tspan>
+            <tspan className={styles.cls-1  x="16.62" y="20">c</tspan>
+            <tspan x="24.5" y="20">k</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(837.06 173.23)">
+            <tspan x="0" y="0">Enter</tspan>
+        </text>
+        <rect className={styles.cls-3  x="778.4" y="202.52" width="66.02" height="56.53"/>
+        <text className={styles.cls-5  transform="translate(793.03 239.47)">
+            <tspan x="0" y="0">Shift</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(200.01 236.65)">
+            <tspan x="0" y="0">X</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(264.74 236.15)">
+            <tspan x="0" y="0">C </tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(330.92 236.65)">
+            <tspan x="0" y="0">V </tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(396.88 236.65)">
+            <tspan x="0" y="0" xml:space="preserve">B  </tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(465.19 236.65)">
+            <tspan x="0" y="0">N</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(528.57 236.15)">
+            <tspan x="0" y="0">M</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(588.95 225.34)">
+            <tspan x="0" y="0">&lt;</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(654.59 226.34)">
+            <tspan x="0" y="0">&gt;</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(725.5 228.17)">
+            <tspan x="0" y="0">?</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(746.72 250.78)">
+            <tspan x="0" y="0">/</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(682 247.95)">
+            <tspan x="0" y="0">.</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(614.79 246.21)">
+            <tspan x="0" y="0">,</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(560.41 169.64)">
+            <tspan x="0" y="0">K</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(624.79 169.64)">
+            <tspan x="0" y="0">L</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(687.66 181.45)">
+            <tspan x="0" y="0">;</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(752.85 189.6)">
+            <tspan x="0" y="0">‘</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(772 165.58)">
+            <tspan x="0" y="0">“</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(706.91 158.33)">
+            <tspan x="0" y="0">:</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(737.14 117.1)">
+            <tspan x="0" y="0">[</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(759.09 95.65)">
+            <tspan x="0" y="0"></tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(733.91 58.12)">
+            <tspan x="0" y="0">-</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(752.46 18.83)">
+            <tspan x="0" y="0">_</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(801.54 50.38)">
+            <tspan x="0" y="0">=</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(815.78 27.77)">
+            <tspan x="0" y="0">+</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(805.7 117.68)">
+            <tspan x="0" y="0">]</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(81.21 104.31)">
+            <tspan x="0" y="0">Q</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(145.21 104.81)">
+            <tspan x="0" y="0">W</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(212.59 104.31)">
+            <tspan x="0" y="0">E</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(278.15 104.81)">
+            <tspan x="0" y="0">R</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(345.36 104.81)">
+            <tspan x="0" y="0">T</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(413.25 104.81)">
+            <tspan x="0" y="0">Y</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(477.44 104.31)">
+            <tspan x="0" y="0">U</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(546.42 104.06)">
+            <tspan x="0" y="0">I</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(610.21 104.31)">
+            <tspan x="0" y="0">O</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(869.49 117.1)">
+            <tspan x="0" y="0">\</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(887.91 95.65)">
+            <tspan x="0" y="0">|</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(675.6 104.31)">
+            <tspan x="0" y="0">P</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(70.77 54.19)">
+            <tspan x="0" y="0">1</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(137.61 53.69)">
+            <tspan x="0" y="0">2</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(203.54 54.19)">
+            <tspan x="0" y="0">3</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(267.77 54.19)">
+            <tspan x="0" y="0">4</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(335.89 54.69)">
+            <tspan x="0" y="0">5</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(400.39 54.69)">
+            <tspan x="0" y="0">6</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(469.01 54.19)">
+            <tspan x="0" y="0">7</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(536.07 54.19)">
+            <tspan x="0" y="0">8</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(601.77 54.19)">
+            <tspan x="0" y="0">9</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(666.95 54.19)">
+            <tspan x="0" y="0">0</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(85.19 31.58)">
+            <tspan x="0" y="0">!</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(22.28 29.43)">
+            <tspan x="0" y="0">~</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(149.91 31.08)">
+            <tspan x="0" y="0">@</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(222.67 31.08)">
+            <tspan x="0" y="0">#</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(283.98 32.08)">
+            <tspan x="0" y="0">$</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(356.11 32.08)">
+            <tspan x="0" y="0">%</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(414.91 32.08)">
+            <tspan x="0" y="0">^</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(485.3 31.08)">
+            <tspan x="0" y="0">&amp;</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(551.05 32.58)">
+            <tspan x="0" y="0">*</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(617.51 31.58)">
+            <tspan x="0" y="0">(</tspan>
+        </text>
+        <text className={styles.cls-5  transform="translate(682.69 31.58)">
+            <tspan x="0" y="0">)</tspan>
+        </text>
+        <rect className={styles.cls-3  x="257.48" y="267.45" width="325.56" height="56.53"/>
+        <text className={styles.cls-6  transform="translate(865.49 31.46)">
+            <tspan x="0" y="0">Ba</tspan>
+            <tspan className={styles.cls-2  x="14.33" y="0">c</tspan>
+            <tspan x="20.77" y="0">k</tspan>
+            <tspan x="-3.26" y="14">Space</tspan>
+        </text>
+    </g>
+</svg>
+    )
+}
