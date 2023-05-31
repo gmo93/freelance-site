f326d56f3e2b5fede2f1fa0e0fac62bea68c680f
gmo93 <omohundrogrant@yahoo.com>
2023-05-30 19:21:39 -0500

    Made menu font thin to match the outline of the animated logo. Makes for a cohesive homepage

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <div className="fixed bottom-0 z-10 p-2 flex flex-wrap justify-center h-14 md:h-20 w-screen items-center text-center bg-omo-fifth text-omo-second font-normal font-sans">
+    <div className="fixed bottom-0 z-10 p-2 flex flex-wrap justify-center h-14 md:h-20 w-screen items-center text-center bg-omo-fifth text-omo-second font-light font-sans">
       <h4 className="mx-3">HOME</h4>
       <h4 className="mx-3">BLOG</h4>
       <h4 className="mx-3">CONTACT</h4>
