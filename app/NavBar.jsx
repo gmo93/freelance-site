7a106dbdf0bbd7a828c1cb5c577efda6e82f28ed
gmo93 <omohundrogrant@yahoo.com>
2023-05-31 13:00:33 -0500

    Animated menu load-in through a customized Tailwind animation titled "loadIn". It can be activated inline through "animate-loadIn"

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,10 +1,10 @@
 export default function NavBar() {
   return (
     <div className="fixed bottom-0 z-10 p-2 flex flex-wrap justify-center h-14 md:h-20 w-screen items-center text-center bg-omo-fifth text-omo-second font-light font-sans">
-      <h4 className="mx-3">HOME</h4>
-      <h4 className="mx-3">BLOG</h4>
-      <h4 className="mx-3">CONTACT</h4>
-      <h4 className="mx-3">EXAMPLES</h4>
+      <h4 className={`mx-3 animate-loadIn`}>HOME</h4>
+      <h4 className={`mx-3 animate-loadIn`}>BLOG</h4>
+      <h4 className={`mx-3 animate-loadIn`}>CONTACT</h4>
+      <h4 className={`mx-3 animate-loadIn`}>EXAMPLES</h4>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,22 @@ module.exports = {
       },
       animation: {
         rotate: 'rotate 5s ease-in-out infinite',
+        loadIn: 'loadIn 1s ease-in',
       },
       keyframes: {
         rotate: {
           '0%, 100%': { transform: 'rotateY(0deg)' },
           '50%': { transform: 'rotateY(180deg)' },
+        },
+        loadIn: {
+          '0%': {
+            transform: 'translateY(30px)',
+            opacity: 0
+          },
+          '100%': {
+            transform: 'translateY(0px)',
+            opacity: 1
+          }
         }
       }
     },
