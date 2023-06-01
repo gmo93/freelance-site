4c3e04bf208f6176d923b75b57d349f1556d0000
gmo93 <omohundrogrant@yahoo.com>
2023-06-01 13:28:14 -0500

    Mobile navbar items were too small, so made font-size larger and margin small on mobile

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,16 +1,16 @@
 export default function NavBar() {
   return (
-    <div className="text-sm md:text-lg fixed bottom-0 z-10 p-2 flex flex-wrap justify-center h-14 md:h-20 w-screen items-center text-center bg-omo-fifth text-omo-second font-light font-sans">
+    <div className="text-md md:text-lg fixed bottom-0 z-10 p-2 flex flex-wrap justify-center h-14 md:h-20 w-screen items-center text-center bg-omo-fifth text-omo-second font-light font-sans">
       <a href="/#home">
-        <h4 className={`mx-3 animate-loadIn`}>HOME</h4>
+        <h4 className={`mx-2 animate-loadIn`}>HOME</h4>
       </a>
       <a href="/services">
-        <h4 className={`mx-3 animate-loadIn`}>SERVICES</h4>
+        <h4 className={`mx-2 animate-loadIn`}>SERVICES</h4>
       </a>
       <a href="/#contact">
-        <h4 className={`mx-3 animate-loadIn`}>CONTACT</h4>
+        <h4 className={`mx-2 animate-loadIn`}>CONTACT</h4>
       </a>
-      <h4 className={`mx-3 animate-loadIn`}>PROJECTS</h4>
+      <h4 className={`mx-2 animate-loadIn`}>PROJECTS</h4>
     </div>
   );
 }
