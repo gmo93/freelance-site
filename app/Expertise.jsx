88719df257b27c1329d37aff5a5b8c79cf4ecd48
gmo93 <omohundrogrant@yahoo.com>
2023-05-31 22:25:27 -0500

    Added an animation to the expertise page to draw more attention and make it a little more fun.

--- app/Expertise.jsx
+++ app/Expertise.jsx
@@ -2,12 +2,23 @@
 
 import { useInView } from "react-intersection-observer";
 import Image from "next/image";
+import {useState} from 'react';
 
 export default function Expertise() {
   const { ref, inView, entry } = useInView({
     threshold: 0.2,
     triggerOnce: true,
   });
+
+  const expArray = ['Web Development', 'Branding', 'UX / UI Design', 'eCommerce', 'SEO', 'Wordpress', 'Shopify', 'Home Page Animation']
+  const [count, setCount] = useState(0);
+  if (count < expArray.length) {
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 1000)
+  } else {
+    setCount(0)
+  }
 
   return (
     <div
@@ -21,18 +32,78 @@ export default function Expertise() {
       </div>
       <div className={`flex flex-wrap w-5/6 items-start`}>
         <ul
-          className={`${inView ? "opacity-1" : "opacity-0"} ${
-            inView ? "translate-x-0" : "translate-x-12"
-          } duration-500 w-3/4 text-white text-xl md:text-4xl lg: text-5xl font-light flex flex-wrap text-omo-fourth`}
+          className={`${inView ? 'opacity-1' : 'opacity-0'} ${
+            inView ? 'translate-x-0' : 'translate-x-12'
+          } duration-500 w-3/4 text-white md:text-4xl lg: text-5xl font-light flex flex-wrap`}
         >
-          <li className={`w-full text-xl md:text-4xl`}>-Web Development</li>
-          <li className={`w-full text-xl md:text-4xl`}>-Branding</li>
-          <li className={`w-full text-xl md:text-4xl`}>-UX / UI Design</li>
-          <li className={`w-full text-xl md:text-4xl`}>-eCommerce</li>
-          <li className={`w-full text-xl md:text-4xl`}>-SEO</li>
-          <li className={`w-full text-xl md:text-4xl`}>-Wordpress</li>
-          <li className={`w-full text-xl md:text-4xl`}>-Shopify</li>
-          <li className={`w-full text-xl md:text-4xl`}>
+          <li
+            className={`w-full ${
+              count === 0
+                ? 'text-omo-fifth translate-x-2'
+                : 'text-white translate-x-0'
+            } text-xl md:text-4xl`}
+          >
+            -Web Development
+          </li>
+          <li
+            className={`w-full ${
+              count === 1
+                ? 'text-omo-fifth translate-x-2'
+                : 'text-white translate-x-0'
+            }  text-xl md:text-4xl`}
+          >
+            -Branding
+          </li>
+          <li
+            className={`w-full ${
+              count === 2
+                ? 'text-omo-fifth translate-x-2'
+                : 'text-white translate-x-0'
+            }  text-xl md:text-4xl`}
+          >
+            -UX / UI Design
+          </li>
+          <li
+            className={`w-full ${
+              count === 3
+                ? 'text-omo-fifth translate-x-2'
+                : 'text-white translate-x-0'
+            }  text-xl md:text-4xl`}
+          >
+            -eCommerce
+          </li>
+          <li
+            className={`w-full ${
+              count === 4
+                ? 'text-omo-fifth translate-x-2'
+                : 'text-white translate-x-0'
+            }  text-xl md:text-4xl`}
+          >
+            -SEO
+          </li>
+          <li
+            className={`w-full ${
+              count === 5
+                ? 'text-omo-fifth translate-x-2'
+                : 'text-white translate-x-0'
+            }  text-xl md:text-4xl`}
+          >
+            -Wordpress
+          </li>
+          <li
+            className={`w-full ${
+              count === 6
+                ? 'text-omo-fifth translate-x-2'
+                : 'text-white translate-x-0'
+            }  text-xl md:text-4xl`}
+          >
+            -Shopify
+          </li>
+          <li
+            className={`w-full ${
+              count === 7 ? 'text-omo-fifth translate-x-2' : 'text-white translate-x-0'
+            }  text-xl md:text-4xl`}
+          >
             -Home Page Animations
           </li>
         </ul>
