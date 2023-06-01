7ce541fd4f8e596fb6a6936fec7c58e1d77ce0b6
gmo93 <omohundrogrant@yahoo.com>
2023-06-01 13:40:39 -0500

    Added phrase to home logo

--- app/Quotes.jsx
+++ app/Quotes.jsx
@@ -6,7 +6,7 @@ import BottomQuote from './BottomQuote';
 
 export default function Quotes() {
   return (
-    <div className="z-5 flex flex-wrap h-5/6 w-full text-xl md:text-5xl justify-center items-center">
+    <div className="z-5 flex flex-wrap h-5/6 w-full text-2xl md:text-5xl justify-center items-center">
       <TopQuote />
       <MiddleQuote />
       <BottomQuote />
