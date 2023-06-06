10bfb57926d7e2743f492e547f3f0d5189ac51b1
gmo93 <omohundrogrant@yahoo.com>
2023-06-05 22:54:05 -0500

    Adjusted projects page to have a white logo aesthetic

---
+++ app/webprojects/page.jsx
@@ -0,0 +1,78 @@
+'use client';
+
+import NavBar from '../NavBar';
+import { useInView } from 'react-intersection-observer';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Services() {
+  const { ref, inView, entry } = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
+  const expArray = [
+    'Web Development',
+    'Branding',
+    'UX / UI Design',
+    'eCommerce',
+    'SEO',
+    'Wordpress',
+    'Shopify',
+    'Home Page Animation',
+  ];
+  const [count, setCount] = useState(0);
+  if (count < expArray.length) {
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 750);
+  } else {
+    setCount(0);
+  }
+
+  const infoHider = (e) => {
+    e.target.parentElement.style.display = 'none';
+  };
+
+  const infoShower = (e) => {
+    console.log((e.target.nextSibling.style.display = 'flex'));
+  };
+
+  return (
+    <div
+      ref={ref}
+      className={`bg-omo-fifth min-h-screen h-auto w-screen flex flex-wrap justify-center md:justify-center items-start pt-10`}
+    >
+      <NavBar />
+      <div className={`w-screen flex pl-10`}>
+        <h1 className={`text-6xl text-omo-second font-bold mb-20`}>Projects</h1>
+      </div>
+      <div
+        className={`grid grid-cols-2 md:grid-cols-3 w-screen h-5/6 flex justify-center items-center`}
+      >
+        <div className={`flex flex-wrap justify-center h-2/3 mb-40 mx-10`}>
+          <Image src="/fmmWhiteLogo.svg" height={300} width={300} />
+        </div>
+        <div
+          className={`flex flex-wrap h-2/3 items-center justify-center mb-40 mx-10`}
+        >
+          <Image src="/omoPhotosWhiteLogo.svg" height={300} width={300} />
+        </div>
+        <div className={`flex flex-wrap justify-center h-2/3 mb-40 mx-10`}>
+          <Image src="/wftkWhiteFullLogo.svg" height={300} width={300} />
+        </div>
+        <div className={`flex flex-wrap justify-center h-2/3 mb-40 mx-10`}>
+          <Image src="/fmmWhiteLogo.svg" height={300} width={300} />
+        </div>
+        <div
+          className={`flex flex-wrap h-2/3 items-center justify-center mb-40 mx-10`}
+        >
+          <Image src="/omoPhotosWhiteLogo.svg" height={300} width={300} />
+        </div>
+        <div className={`flex flex-wrap justify-center h-2/3 mb-40 mx-10`}>
+          <Image src="/wftkWhiteFullLogo.svg" height={300} width={300} />
+        </div>
+      </div>
+    </div>
+  );
+}
